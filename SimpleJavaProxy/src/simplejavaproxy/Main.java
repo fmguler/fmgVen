409a37e864b1410b71434ce6a7850e273787b062
Fatih Mehmet Güler <fmguler@gmail.com>
2010-04-20 19:14:43 +0000

    SimpleJavaProxy ilk import. Basit bir java proxy server. Tarayıcıdan bağımsız cookie'leri yönetmek için oluşturulan bir yardımcı proje. Bu halinde safari key'ini override ediyor.

---
+++ SimpleJavaProxy/src/simplejavaproxy/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simplejavaproxy;
+
+/**
+ *
+ * @author fmguler
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //SimpleProxy.main(new String[]{"9999","127.0.0.1","8888"});
+        SimpleProxy.main(new String[]{"9999"});
+    }
+
+}
